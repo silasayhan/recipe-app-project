fa0f5b5c942d7361eab2fc588da00f6bde9f8878
Sude <silasayhans@gmail.com>
2026-01-08 03:13:08 +0300

    Backend içindeki node_modules dosyası silindi.

--- backend/dist/auth/roles.decorator.d.ts
+++
@@ -1,2 +0,0 @@
-import { UserRole } from '../users/user.entity';
-export declare const Roles: (...roles: UserRole[]) => import("@nestjs/common").CustomDecorator<string>;
--- backend/dist/categories/category.entity.d.ts
+++
@@ -1,7 +0,0 @@
-import { Recipe } from '../recipes/recipe.entity';
-export declare class Category {
-    id: number;
-    name: string;
-    description: string;
-    recipes: Recipe[];
-}
